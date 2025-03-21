e77e348b28596efedd785e22d8d10f278173f265
Mayank Rajput <mayankrajput98.mr@gmail.com>
2025-03-21 12:13:04 +0000

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("Portfolio loaded with style!");
